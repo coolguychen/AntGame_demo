217bf91fb536b3a4f753bc0af71b8f1abf488b9c
coolguychen <yuchen_616@163.com>
2021-09-17 20:46:45 +0800

    version 0.0

---
+++ src/Ant.java
@@ -0,0 +1,45 @@
+public class Ant {
+    int id;
+    int velocity;
+    int location;
+    int hit;
+
+    //setter and getter
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getVelocity() {
+        return velocity;
+    }
+
+    public void setVelocity(int velocity) {
+        this.velocity = velocity;
+    }
+
+    public int getLocation() {
+        return location;
+    }
+
+    public void setLocation(int location) {
+        this.location = location;
+    }
+
+    public int getHit() {
+        return hit;
+    }
+
+    public void setHit(int hit) {
+        this.hit = hit;
+    }
+
+    //creep
+    void creep(){
+        //todo 还没写完
+    }
+}
+
---
+++ src/stick.java
@@ -0,0 +1,17 @@
+public class stick {
+    int length;
+
+    //getter and setter
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    //判断是否蚂蚁是否会掉下去
+    boolean isOut(Ant ant){
+        //todo
+    }
+}
